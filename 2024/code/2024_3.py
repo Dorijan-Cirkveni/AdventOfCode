545d8510798cac9369d5c037b44f029e906c834b
Dorijan-Cirkveni <33494690+Dorijan-Cirkveni@users.noreply.github.com>
2024-12-04 10:03:42 +0100

    Create 2024_3.py

---
+++ 2024/code/2024_3.py
@@ -0,0 +1,78 @@
+import re
+from collections import Counter, defaultdict, deque
+
+
+def read(filepath):
+    M=[]
+    with open(filepath,'r') as file:
+        for line in file:
+            if not line:
+                continue
+            L=line[:-1]
+            M.append(L)
+    return M
+
+def get_locations(s,pattern,sentinel=False):
+    L=[-1] if sentinel else []
+    for m in re.finditer(r'mul\(\d{1,3},\d{1,3}\)',s):
+        L.append(m.pos)
+
+def check_line(s):
+    res=0
+    for m in re.finditer(r'mul\(\d{1,3},\d{1,3}\)',s):
+        a,b=m.group()[4:][:-1].split(',')
+        res+=int(a)*int(b)
+    return res
+
+def merge(starts:list,ends:list):
+
+
+def check_line_2(s):
+    res=0
+    starts=get_locations(s,'do()', True)
+    ends=get_locations(s,'don\'t()')
+    for m in re.finditer(r'mul\(\d{1,3},\d{1,3}\)',s):
+        a,b=m.group()[4:][:-1].split(',')
+        res+=int(a)*int(b)
+    return res
+
+
+def process_1(M):
+    res=0
+    while M:
+        L=M.pop()
+        res+=check_line(L)
+    return res
+
+def make_counter(A):
+    D=defaultdict(int)
+    while A:
+        D[A.pop()]+=1
+    return D
+
+def process_2(M):
+    res = 0
+    while M:
+        L = M.pop()
+        res += check_line_2(L)
+    return res
+
+
+TASK=__file__.split('\\')[-1][:-3]
+
+def runprocess(process:callable):
+    test=["mul(2,2) mul(1,2)"],
+    result=process(*test)
+    print(result)
+    inputbase=f"..\\inputs\\{TASK}.txt"
+    data=read(inputbase),
+    result=process(*data)
+    print(result)
+
+def main():
+    runprocess(process_2)
+    return
+
+
+if __name__ == "__main__":
+    main()
