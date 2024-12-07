4dfd59c3637646f554ce336866bd280c6ffa0280
Mouse Potato Does Stuff <dorijan.cirkveni@engineer.com>
2024-12-07 22:58:54 +0100

    wasd

--- 2024/code/2024_7.py
+++ 2024/code/2024_7.py
@@ -4,104 +4,53 @@ def read(filepath):
     return M
 
 
-def preprocess(M):
-    for e in M:
-        e += ' '
-    M.append(' ' * len(M[-1]))
-    RES = [list(e) for e in M]
-    for E in RES:
-        E.append(' ')
-    return RES
+def preprocess(s:str):
+    ca,cb=s.split(': ')
+    return int(ca),[int(e) for e in cb.split()]
 
+def check_forward(target:int,parts:list):
+    L={0:""}
+    for cur in parts:
+        NL={}
+        for e in L:
+            v=L[e]
+            if e+cur<=target:
+                NL[e+cur]=v+'+'
+            if e*cur<=target:
+                NL[e*cur]=v+'*'
+        L=NL
+    return L.get(target,'')
 
-DIRECTIONS = {
-    (-1, 0): 'W',
-    (0, -1): 'A',
-    (0, 1): 'S',
-    (1, 0): 'D'
-}
+def verify(data,ops,res):
+    ops=list(ops)
+    while data:
+        cur=data.pop()
+        op=ops.pop()
+        if op=='+':
+            res-=cur
+        else:
+            if res%cur!=0:
+                return False
+            res//=cur
+    return res==0
 
-
-class Matrix:
-    def __init__(self, M):
-        self.M = preprocess(M)
-        self.start = -1, -1
-        for i, s in enumerate(self.M):
-            if "^" in s:
-                self.start = i, s.index('^')
-                break
-        self.set(*self.start, '.')
-        return
-
-    def get(self, i, j):
-        return self.M[i][j]
-
-    def set(self, i, j, v):
-        self.M[i][j] = v
-
-    def getNex(self, i, j, di, dj):
-        nei = i + di
-        nej = j + dj
-        return nei, nej
-
-    def step(self, cur, dir):
-        neigh = -1, -1
-        for i in range(4):
-            neigh = self.getNex(*cur, *dir)
-            if self.get(*neigh) != "#":
-                cur = neigh
-                break
-            dir = dir[1], -dir[0]
-        return cur, dir
-
-    def count(self, cur):
-        res = 1
-        delta = -1, 0
-        while self.get(*cur) != ' ':
-            self.set(*cur, DIRECTIONS[delta])
-            cur, delta = self.step(cur, delta)
-            res += self.get(*cur) == '.'
-        return res
-
-    def log(self, cur, delta=(-1, 0)):
-        LD = set()
-        while self.get(*cur) != ' ':
-            new, newdelta = self.step(cur, delta)
-            E = (cur, newdelta, new)
-            if E in LD:
-                return None
-            LD.add(E)
-            cur, delta = new, newdelta
-        return {e[2] for e in LD}
-
-    def all_loops(self, start):
-        LD = self.log(start)
-        res = 0
-        i=0
-        print(len(LD))
-        for new in LD:
-            temp=self.get(*new)
-            self.set(*new, '#')
-            RES = self.log(start)
-            if not RES:
-                res += 1
-            self.set(*new, temp)
-            i+=1
-            if i%100==0:
-                print((i,len(LD)))
-        return res
 
 
 def process_1(data):
-    M = Matrix(data)
-    res = M.count(M.start)
+    res=0
+    res0=0
+    for entry in data:
+        processed=preprocess(entry)
+        ops=check_forward(*processed)
+        if ops:
+            res+=processed[0]
+            if not verify(processed[1][:],ops,processed[0]):
+                raise Exception(processed)
     return res
 
 
 def process_2(data):
-    M = Matrix(data)
-    res=M.all_loops(M.start)
-    return res
+    return process_1(data)
 
 
 TASK = __file__.split('\\')[-1][:-3]
@@ -110,25 +59,18 @@ TASK = __file__.split('\\')[-1][:-3]
 def runprocess(process: callable):
     inputbase = f"..\\inputs\\{TASK}.txt"
     data = read(inputbase)
-    datatemp = '''....#.....
-.........#
-..........
-..#.......
-.......#..
-..........
-.#..^.....
-........#.
-#.........
-......#...'''.split('\n')
-    result = process(datatemp)
-    print(result)
     result = process(data)
     print(result)
 
 
 def main():
+    c=verify([1,1,1],'+++',4)
     runprocess(process_2)
     return
+
+'''
+5540634308465 too high
+'''
 
 
 if __name__ == "__main__":
