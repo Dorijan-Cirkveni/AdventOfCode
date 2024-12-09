f9773d1c46e1954792d4564bbaa3f7203affc612
Dorijan-Cirkveni <33494690+Dorijan-Cirkveni@users.noreply.github.com>
2024-12-09 09:42:19 +0100

    Update 2024_8.py

--- 2024/code/2024_8.py
+++ 2024/code/2024_8.py
@@ -1,3 +1,4 @@
+import math
 from collections import defaultdict
 
 
@@ -36,15 +37,32 @@ class AntennaField:
                 continue
             res.add((x,y))
 
-    def check_ant_type(self,mark:str, res:set):
+    def check_antray(self,st_x,st_y,dx,dy,res:set):
+        while st_x in range(self.n) and st_y in range(self.m):
+            res.add((st_x,st_y))
+            st_x+=dx
+            st_y+=dy
+
+    def check_antline(self,x1:int,y1:int,x2:int,y2:int,res:set):
+        dx=x2-x1
+        dy=y2-y1
+        scale=math.gcd(dx,dy)
+        dx//=scale
+        dy//=scale
+        self.check_antray(x2,y2,dx,dy,res)
+        self.check_antray(x2,y2,-dx,-dy,res)
+
+
+    def check_ant_type(self,mark:str, res:set, is_line=False):
+        func:callable=(self.check_antpair,self.check_antline)[is_line]
         L:list=self.antennas.get(mark,[])
         for i,e in enumerate(L):
             for j in range(i):
-                self.check_antpair(*e,*L[j],res=res)
+                func(*e,*L[j],res=res)
 
-    def get_all_targets(self,res:set):
+    def get_all_targets(self,res:set, is_line=False):
         for e in self.antennas:
-            self.check_ant_type(e,res)
+            self.check_ant_type(e,res, is_line)
         return len(res)
 
 
@@ -57,7 +75,10 @@ def process_1(data):
 
 
 def process_2(data):
-    return process_1(data)
+    AF=AntennaField(data)
+    res_set=set()
+    res=AF.get_all_targets(res_set,True)
+    return res
 
 
 TASK = __file__.split('\\')[-1][:-3]
