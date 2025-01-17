11a3f4024c33b7be38ee007f4ba4ad64f4411475
Dorijan-Cirkveni <33494690+Dorijan-Cirkveni@users.noreply.github.com>
2025-01-17 08:30:36 +0100

    Update 2024_22.py

--- 2024/code/2024_22.py
+++ 2024/code/2024_22.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 def read(filepath):
     M=None
     try:
@@ -10,19 +13,73 @@ def read(filepath):
 
 
 def preprocess(s:str):
-    return s
+    return int(s)
 
+MOD=16777216
+
+def step1(n):
+    n^=n<<6
+    n%=MOD
+    return n
+def step2(n):
+    n^=n>>5
+    n%=MOD
+    return n
+def step3(n):
+    n^=n<<11
+    n%=MOD
+    return n
+def all_step(n):
+    a=step1(n)
+    b=step2(a)
+    c=step3(b)
+    return c
+
+def full(n,steps=2000):
+    for _ in range(steps):
+        n=all_step(n)
+    return n
 
 
 def process_1(data):
     res=0
     for entry in data:
-        processed=preprocess(entry)
+        processed:int=preprocess(entry)
+        res_temp=full(processed)
+
+        print(f"{processed}: {res_temp}")
+        res+=res_temp
     return res
 
 
+def log_price_change_sequence_rewards(start_number,profit_log:defaultdict[tuple,int],steps=2000):
+    changes=tuple()
+    found={}
+    last_price=start_number%10
+    for _ in range(steps):
+        next_number=all_step(start_number)
+        next_price=next_number%10
+        delta=next_price-last_price
+        changes=changes[-3:]+(delta,)
+        if len(changes)==4 and changes not in found:
+            found[changes]=next_price
+            profit_log[changes]+=next_price
+        last_price=next_price
+        start_number=next_number
+    return found
+
+
+
 def process_2(data):
-    return process_1(data)
+    res=0
+    profit_log=defaultdict(int)
+    for entry in data:
+        processed:int=preprocess(entry)
+        res_temp=log_price_change_sequence_rewards(processed,profit_log)
+        print(set(res_temp.values()))
+    print(set(profit_log.values()))
+    res=max(profit_log.values())
+    return res
 
 
 TASK = __file__.split('\\')[-1][:-3]
@@ -34,6 +91,7 @@ def runprocess_withinputfrom(process: callable, suffix:str=""):
     if data is not None:
         result = process(data)
         print(f"Result for {suffix}: {result}")
+        input("Press Enter to continue:")
 
 
 def runprocess(process: callable, input_files=None):
@@ -44,7 +102,7 @@ def runprocess(process: callable, input_files=None):
 
 
 def main():
-    runprocess(process_2,["t",""])
+    runprocess(process_2,["tt","t2",""])
     return
 
 
