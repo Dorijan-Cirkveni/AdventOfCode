da450320b1f46774268dbd5b2420a7476224f4f1
Dorijan Cirkveni <33494690+Dorijan-Cirkveni@users.noreply.github.com>
2025-12-02 11:44:52 +0100

    finished ac25 part 1 in java

---
+++ 2025/code/ac2025.java
@@ -0,0 +1,84 @@
+package code;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.channels.Pipe.SourceChannel;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ac2025 {
+    public static void main(String[] args) {
+        ac2025 main=new ac2025();
+        main.submain(0);
+    }
+
+    public String step1(List<String> ls) {
+        return "Running step 1";// Default preprocessing (identity function)
+    }
+
+    public String step2(List<String> ls) {
+        return "Running step 2";// Default preprocessing (identity function)
+    }
+
+    public void submain(int tasknum){
+        Scanner sc = new Scanner(System.in);
+        int solution_num= sc.nextInt();
+        String files=sc.nextLine().trim();
+        String[] filelist = (files.length()==0)?new String[]{"","t"}:files.split(" ");
+        sc.close();
+        System.out.println(">>"+String.join(",",filelist));
+        this.runProcess(tasknum, solution_num, filelist);
+    }
+
+    private void runProcess(int tasknum, int process, String[] inputFiles) {
+        if (inputFiles == null) {
+            inputFiles = new String[]{"","t"};
+        }
+        for (String suffix : inputFiles) {
+            this.runProcessWithInputFrom(tasknum, process, suffix);
+        }
+    }
+
+    private void runProcessWithInputFrom(int tasknum, int process, String suffix) {
+        String task = "2025_"+tasknum;
+        String address = "inputs\\"+task+suffix+".txt";
+        System.out.println(address);
+        List<String> data = this.read(address);
+        if (data == null) {System.out.printf("Data missing for %s!\n",suffix); return;}
+        String result = "No result.";
+        switch (process) {
+            case 1:
+                result = this.step1(data);
+                break;
+        
+            case 2:
+                result = this.step2(data);
+                break;
+
+            default:
+                break;
+        }
+        System.out.printf("Result for %s: %s%n\n", suffix, result);
+    }
+
+    private List<String> read(String filepath) {
+        List<String> lines = null;
+        try {
+            FileReader fread=new FileReader(filepath);
+            BufferedReader reader = new BufferedReader(fread);
+            System.out.println("Reading...");
+            lines = new ArrayList<>();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+            reader.close();
+            fread.close();
+        } catch (IOException err) {
+            System.err.println("IO Exception:"+err.getMessage());
+        }
+        return lines;
+    }
+
+}
--- 2025/code/ac2025_1.java
+++ 2025/code/ac2025_1.java
@@ -1,82 +1,41 @@
 package code;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.channels.Pipe.SourceChannel;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
-public class ac2025_1 {
+public class ac2025_1 extends ac2025{
+    int SIZE=99+1;
     public static void main(String[] args) {
-        String[] filelist = new String[]{"t"};
-        runProcess(2, filelist);
+        ac2025_1 main=new ac2025_1();
+        main.submain(1);
     }
 
-    private static void runProcess(int process, String[] inputFiles) {
-        if (inputFiles == null) {
-            inputFiles = new String[]{""};
-        }
-        for (String suffix : inputFiles) {
-            runProcessWithInputFrom(process, suffix);
-        }
-    }
-
-    private static void runProcessWithInputFrom(int process, String suffix) {
-        Solution sol=new Step2();
-        if (process==1) sol=new Step1();
-        String task = "ac2025_1";
-        String inputBase = String.format("inputs\\%s%s.txt", task, suffix);
-        List<String> data = read(inputBase);
-        if (data != null) {
-            System.out.println(data.size());
-            String result = sol.solve(data);
-            System.out.println("Result for "+suffix+":"+"\n"+result);
-        } else {
-            System.out.printf("Data missing for %s!\n",suffix);
-        }
-    } //
-
-    private static List<String> read(String filepath) {
-        List<String> lines = null;
-        try (BufferedReader reader = new BufferedReader(new FileReader(filepath))) {
-            lines = new ArrayList<>();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                lines.add(line);
-            }
-        } catch (IOException err) {
-            System.err.println(err.getMessage());
-        }
-        return lines;
-    }
-
-}
-
-class Solution {
-
-    public String solve(List<String> ls) {
-        return "Error";// Default preprocessing (identity function)
-    }
-}
-
-class Step1 extends Solution {
-
-    public String solve(List<String> ls) {
+    public String step1(List<String> ls) {
         int count=0;
         int status=50;
         for (String entry:ls){
             String raw=entry.substring(1);
             Integer num=Integer.parseInt(raw);
             if (entry.startsWith("R")) num=-num;
-            status+=num;status%=100;
+            status+=num;status%=this.SIZE;
             if (status==0) count++;
         }
         return ""+count;
     }
 
-}
-
-class Step2 extends Step1 {
-    
+    public String step2(List<String> ls) {
+        int count=0;
+        int status=50;
+        for (String entry:ls){
+            String raw=entry.substring(1);
+            Integer num=Integer.parseInt(raw);
+            count+=num/this.SIZE;
+            num%=this.SIZE; if (num==0) continue;
+            int prefix=entry.startsWith("L")?this.SIZE-1:1;
+            while (num>0) {num--;
+                status+=prefix;
+                status%=this.SIZE;
+                if (status==0) count++;
+            }
+        }
+        return ""+count;
+    }
 }
